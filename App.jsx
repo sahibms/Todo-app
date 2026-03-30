b8e38a0c3891fc3903dd8703030900ddc8f52fc2
sahibms <sahibms12345@gmail.com>
2026-03-30 10:48:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Todo from "./Todo";
+import "./App.css"
+
+function App() {
+  return(
+    
+    <div className="app">
+      <h2>Todo App</h2>
+    <Todo/>
+    </div>
+  )
+}
+
+export default App;
